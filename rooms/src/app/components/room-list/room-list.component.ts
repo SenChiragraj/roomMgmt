3ef94897028ca98f20ad3edacc1749f80b2d36ee
Chiragraj Sen <114171989+SenChiragraj@users.noreply.github.com>
2025-01-29 03:38:11 +0000

    Sort

--- rooms/src/app/components/room-list/room-list.component.ts
+++ rooms/src/app/components/room-list/room-list.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core'
+import { Component, OnChanges, SimpleChanges, OnInit } from '@angular/core'
 import { Room } from '../../models/room'
-import { OnInit } from '@angular/core'
 import { RoomService } from '../../services/room.service'
 import { Router } from '@angular/router'
 
@@ -10,51 +9,56 @@ import { Router } from '@angular/router'
   styleUrls: ['./room-list.component.scss']
 })
 export class RoomListComponent implements OnInit {
-  constructor (private service: RoomService, private router: Router) {}
-
   rooms: Room[] = []
   errorMessage: string = ''
   searchText: string = ''
   timeOut: any
 
+  constructor (private service: RoomService, private router: Router) {}
+
   ngOnInit (): void {
     this.loadRooms()
   }
 
-  loadRooms () {
+  loadRooms (): void {
     this.service.getRooms().subscribe({
       next: (data: Room[]) => {
-        this.rooms = data
+        this.rooms = data.filter(room => room.name.includes(this.searchText))
       },
       error: () => {
-        this.errorMessage = 'Error in fetching  all rooms'
+        this.errorMessage = 'Error in fetching all rooms'
       }
     })
   }
 
-  onDelete (id: number | undefined) {
+  onDelete (id: number | undefined): void {
     this.service.deleteRoom(id).subscribe({
       next: () => {
         this.loadRooms()
       },
-      error: error => {
+      error: () => {
         this.errorMessage = 'Error in deleting room'
       }
     })
   }
 
-  onEdit (id: number | undefined) {
-    console.log('edit')
-
+  onEdit (id: number | undefined): void {
     this.router.navigate([`/edit-rooms/${id}`])
   }
 
-  onSearch () {
+  onSearch (): void {
     if (this.timeOut) {
       clearTimeout(this.timeOut)
     }
     this.timeOut = setTimeout(() => {
       console.log(this.searchText)
+      this.loadRooms()
     }, 500)
+  }
+
+  onSort () {
+    this.rooms.sort((a, b) => {
+      return Math.max(a.name.localeCompare(b.name), a.capacity - b.capacity)
+    })
   }
 }
